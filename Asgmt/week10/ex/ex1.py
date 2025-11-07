179a6f9013b947ea26f6ffc8254f3ca8e4f09496
Ryu <rhm0202@koreatech.ac.kr>
2025-11-07 11:13:56 +0900

    10주차 예제1

--- Asgmt/week10/ex/ex1.py
+++ Asgmt/week10/ex/ex1.py
@@ -1,22 +1,4 @@
-import cv2
+import cv2 as cv
+import sys
 
-def main():
-    camera = cv2.VideoCapture(0)
-    camera.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-    camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-    camera.set(cv2.CAP_PROP_FPS, 30)
-
-    while camera.isOpened():
-        ret, image = camera.read()
-        if not ret:
-            break
-        cv2.imshow('camera test', image)
-
-        if cv2.waitKey(1) == ord('q'):
-            break
-
-    camera.release()
-    cv2.destroyAllWindows()
-
-if __name__ == '__main__':
-    main()
+img = cv.imread()
---
+++ Asgmt/week10/week10_1.py
@@ -0,0 +1,33 @@
+import cv2
+
+def main():
+    camera = cv2.VideoCapture(0)
+    camera.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+    camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+    camera.set(cv2.CAP_PROP_FPS, 30)
+    
+    face_cascade = cv2.CascadeClassifier("../etc/haarcascade_frontalface_default.xml")
+
+    while camera.isOpened():
+        ret, image = camera.read()
+        if not ret:
+            break
+        
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray)
+        
+        for (x, y, w, h) in faces:
+            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        
+        image = cv2.flip(image, 0)
+        
+        cv2.imshow('camera test', image)
+
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+    camera.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
